3f00e8892806961f6b60f0cfd59a10a13649c03b
PavanBandichode <pavan.bandichode@gmail.com>
2022-01-04 19:58:51 +0530

    assign level 2 : assign 3

---
+++ block-BJaaii/code/script.js
@@ -0,0 +1,14 @@
+class Stack {
+  constructor(stack) {
+    this.stack = stack;
+  }
+  push(data) {}
+  pop(data) {}
+  peak(data) {}
+  reverse(data) {}
+  isEmpty(data) {}
+  displayStack(data) {}
+  get length() {
+    return this.length;
+  }
+}
